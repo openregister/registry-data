60a9c3ec251cf8ed17b76366b42636e37993d362
psd <psd@whatfettle.com>
2015-11-28 16:40:01 +0000

    Added registry register

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -15,3 +15,15 @@ def test_registry_key_matches_filename(registry):
 @pytest.mark.parametrize('registry', registries)
 def test_registry_is_used_by_a_register(registry):
     assert registry in registry_registers
+
+
+@pytest.mark.parametrize('registry', registries)
+def test_registry_keys_are_known_fields(registry):
+    for field in registries[registry].keys:
+        assert field in fields
+
+
+@pytest.mark.parametrize('registry', registries)
+def test_registry_registries_are_register_registries(registry):
+    for field in registries[registry].keys:
+        assert field in registers['registry'].fields
